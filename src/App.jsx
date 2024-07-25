8697d58032a998721c49f284feabe34e3549a7a8
Parth Chawande <parthrc00@gmail.com>
2024-07-25 14:38:01 +0530

    added grapesjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import "./App.css";
 
+import "grapesjs/dist/css/grapes.min.css";
+import GrapesJS from "grapesjs";
+import { useEffect } from "react";
+
 function App() {
-  return <div>Hello</div>;
+  // grapesjs setup
+  useEffect(() => {
+    const editor = GrapesJS.init({
+      container: "#gjs",
+      fromElement: true,
+      showOffsets: true,
+      noticeOnUnload: false,
+      storageManager: false,
+      // plugins here
+      plugins: [],
+      // to apply styles to individual components
+      selectorManager: {
+        componentFirst: true,
+      },
+      parser: {
+        optionsHtml: {
+          allowScripts: true,
+        },
+      },
+    });
+  }, []);
+  return <div id="gjs">Hello</div>;
 }
 
 export default App;
